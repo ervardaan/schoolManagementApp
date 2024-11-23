3b27ecae1a6acabf2998291854e07f247a5df6aa
vardaan <vardaan2807@gmail.com>
2024-11-23 13:32:38 -0600

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sudo apt-get remove nodejs
+sudo apt autoremove
+sudo apt-get install -y mongodb
+npm install express
+nvm install 17
